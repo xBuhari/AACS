d39cbc53021bb585e072773dc479461e4e3ef760
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 03:28:23 +0300

    chat

--- src/main/java/pw/xbuhari/www/aacs/Utils.java
+++ src/main/java/pw/xbuhari/www/aacs/Utils.java
@@ -1,15 +1,29 @@
 package pw.xbuhari.www.aacs;
 
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 public class Utils {
 
-    public static String cnf(String section) {
-        return clr(Main.getPlugin().getConfig().getString(section, ""));
-    }
-
     public static String clr(String txt) {
         return ChatColor.translateAlternateColorCodes('&', txt);
+    }
+
+    public static void sndList(Player player, String cnf, String[]... replaces) {
+        if (!Main.getPlugin().getConfig().isSet(cnf)) {
+            return;
+        }
+        for (String _msg : Main.getPlugin().getConfig().getStringList(cnf)) {
+            if (replaces.length>0) {
+                String msg = clr(_msg);
+                for (String[] _replaces : replaces) {
+                    msg = msg.replace(_replaces[0], _replaces[1]);
+                }
+                player.sendMessage(msg);
+                continue;
+            }
+            player.sendMessage(clr(_msg));
+        }
     }
 
 }
--- src/main/java/pw/xbuhari/www/aacs/listeners/ChatListener.java
+++ src/main/java/pw/xbuhari/www/aacs/listeners/ChatListener.java
@@ -1,15 +1,28 @@
 package pw.xbuhari.www.aacs.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import pw.xbuhari.www.aacs.Main;
+import pw.xbuhari.www.aacs.Utils;
+
+import java.util.UUID;
 
 public class ChatListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onChat(AsyncPlayerChatEvent e) {
-
+        if (e.isCancelled()) {
+            return;
+        }
+        UUID playerUUID = e.getPlayer().getUniqueId();
+        Player player = e.getPlayer();
+        if (!Main.getPlugin().getAfkDB().hasPlayer(playerUUID)) {
+            return;
+        }
+        e.setCancelled(true);
+        Utils.sndList(player, "lang.chat_protect_message", new String[]{"%block%", String.valueOf(Math.round(Main.getPlugin().getConfig().getDouble("settings.how_many_blocks_move_needed", 1.0)))});
     }
-
 }
